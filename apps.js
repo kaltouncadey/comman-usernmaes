73ce592ae8a9e9a6f88087a33bcb695b9914d239
kaltouncadey <kaltuunaadam35@gmail.com>
2022-10-05 04:44:55 -0700

    is very easy js

---
+++ apps.js
@@ -0,0 +1,117 @@
+// // // let users = {
+// // //           id : 1,
+// // //           name : 'xamdi',
+// // //           tel : 387387843,
+// // // };
+// // // // console.log(users)
+
+
+
+// // // //  console.log  typeof persone.kad rabtid in ad ogatid typeofkisa ayd ku qorysan
+
+
+// // let tiigsi = {
+// //           id : 1,
+// //           name : cadey,
+// //           tel : 8998763,
+// //           address : xawdle,
+// // };
+// // console.log(tiigsi) 
+
+// // const namess = [{
+// //           id : 1,
+// //           name : 'xamda'
+// //           tell : 99867,
+// //           isadmin : false
+// //  },
+
+
+// //           {
+// //                     id : 2,
+// //                     name :'yaasin',
+// //                     tell : 767834,
+// //                     isadmin :false,
+// //           },
+
+// //           {
+// //               id : 3,
+// //               name :'xikmaa',
+// //               tell : 87963,
+// //               isadmin : false ,
+// //           },];
+// //           console.log(namess)
+
+// //           const .username
+
+
+
+// let Person = [
+//           {
+//                     id : 1,
+//                    name : 'cady',
+//                    phone : 639843,
+//                    username : "kaltuuncaday",
+//           },
+
+//           {
+//                  id : 2,
+//                  name : 'xamda',
+//                  phone : 897896,
+//                  username : "salmacaday",
+//           },
+
+//          {
+//           id : 3,
+//           name : 'yasin',
+//           phone : 976769,
+//           username : "nadiiracaday",
+//          },
+
+// ];
+
+// console.log(Person)
+
+
+
+
+// let user = Person[1].username = "nabiihayoonis"
+// console.log([user],[Person]);
+
+
+// // const tii = cary [1].username ('true')
+
+
+
+
+
+const product = {
+          id : 1,
+          productName: "Kamasaro",
+          price : 2
+}  
+
+
+
+const products = [
+         {
+          id : 1,
+          productName: "Kamasaro",
+          price : 2
+         } ,
+
+         {
+          id : 2,
+          productName: "Kamasaro",
+          price : 24
+         },
+
+]
+
+
+// const x = products[0].productName
+
+const y = products[1].price
+
+console.log(y)
+
+console.log(products)
